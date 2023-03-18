8759575d3096d48c909fff7fc52da35a3ed5b5de
Erertao <1793009453@qq.com>
2023-03-18 21:27:40 +0800

    我的界面ver1.0

--- pages/my/my.vue
+++ pages/my/my.vue
@@ -1,6 +1,30 @@
 <template>
-	<view>
-		
+	<view class="titleStyle">
+		我的
+	</view>
+	<view class="name">
+		<view class="nameStyle1">姓名</view>
+		<view style="width: 50%;margin: auto;">
+			<view class="nameStyle">{{adminname}}</view>
+		</view>
+	</view>
+	<view class="name">
+		<view class="nameStyle1">手机号</view>
+		<view style="width: 50%;margin: auto;">
+			<view class="nameStyle">{{phonenumber}}</view>
+		</view>
+	</view>
+	<view class="name">
+		<view class="nameStyle1">修改密码</view>
+		<view style="width: 50%;margin: auto;">
+			<view class="nameStyle">{{adminname}}</view>
+		</view>
+	</view>
+	<view class="name1">
+		<view class="nameStyle1">请假报备</view>
+		<view style="width: 50%;margin: auto;">
+			<view class="nameStyle">{{adminname}}</view>
+		</view>
 	</view>
 </template>
 
@@ -8,7 +32,8 @@
 	export default {
 		data() {
 			return {
-				
+				adminname: '张三',
+				phonenumber: '130****9219'
 			}
 		},
 		methods: {
@@ -18,5 +43,37 @@
 </script>
 
 <style>
-
+	.titleStyle{
+		text-align: center;
+		margin-top: 30rpx;
+		font-size: 40rpx;
+		margin-bottom: 20rpx;
+	}
+	.name{
+		width: 100%;
+		height: 80rpx;
+		flex-flow: row;
+		display: flex;
+		border-top: 1upx solid gray;
+	}
+	.name1{
+		width: 100%;
+		height: 80rpx;
+		flex-flow: row;
+		display: flex;
+		border-top: 1upx solid gray;
+		border-bottom: 1upx solid gray;
+	}
+	.nameStyle1{
+		width: 50%;
+		font-size: 35rpx;
+		margin: auto;
+		margin-left: 10rpx;
+	}
+	.nameStyle{
+		text-align: right;
+		font-size: 35rpx;
+		margin: auto;
+		margin-right: 10rpx;
+	}
 </style>
