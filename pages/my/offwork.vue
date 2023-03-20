23e10b8011579f437e5cc74e570f3873fa14db0e
Hariki <54800204@qq.com>
2023-03-20 20:43:52 +0800

    请假页面ver1.1(输入限制，选择日期，提交)

--- pages/my/offwork.vue
+++ pages/my/offwork.vue
@@ -2,24 +2,86 @@
 	<view>
 		<view class="reason">
 			<text style="font-size: 35rpx;">事宜</text>
-			<textarea class="text-box" placeholder="详情描述"  @input="sumfontnum">
+			<textarea style="word-break:break-all word-wrap:break-word" class="text-box" placeholder="详情描述" maxlength="200" @input="sumfontnum">
 			</textarea>
 			<text class="currentWordNumber">{{fontNum}}/200</text>
 		</view>
+		<view class="begin">
+			<view class="begin-word">开始时间</view>
+			<view class="begin-time">
+				<picker mode="date" :value="date" :start="startDate" :end="endDate" @change="bindDateChangebegin">
+					<view class="begin-input">{{begindate}}</view>
+				</picker>
+			</view>
+		</view>
+		<view class="end">
+			<view class="end-word">结束时间</view>
+			<view class="end-time">
+				<picker mode="date" :value="date" :start="startDate" :end="endDate" @change="bindDateChangeend">
+					<view class="end-input">{{enddate}}</view>
+				</picker>
+			</view>
+		</view>
+		<button class="submit-btn" type="default" @tap="submitAll">提交</button>
 	</view>
 </template>
 
 <script>
 	export default {
 		data() {
+			const currentDate = this.getDate({
+				format:true
+			})
+			
 			return {
 				fontNum:0,
+				begindate:currentDate,
+				enddate:currentDate,
+				info:"",
+			}
+		},
+		computed: {
+			startDate() {
+				return this.getDate('start');
+			},
+			endDate() {
+				return this.getDate('end');
 			}
 		},
 		methods: {
 			sumfontnum(e) {
 				this.fontNum = e.detail.value.length
-				console.log(this.fontNum)
+				this.info = e.detail.value
+				
+				// console.log(this.fontNum)
+			},
+			bindDateChangebegin: function(e) {
+				this.begindate = e.detail.value
+				// console.log(this.begindate)
+			},
+			bindDateChangeend: function(e){
+				this.enddate = e.detail.value
+				// console.log(this.enddate)
+			},
+			getDate(type) {
+				const date = new Date();
+				let year = date.getFullYear();
+				let month = date.getMonth() + 1;
+				let day = date.getDate();
+	
+				if (type === 'start') {
+					year = year - 60;
+				} else if (type === 'end') {
+					year = year + 2;
+				}
+				month = month > 9 ? month : '0' + month;
+				day = day > 9 ? day : '0' + day;
+				return `${year}-${month}-${day}`;
+			},
+			submitAll(){
+				console.log(this.info)
+				console.log(this.begindate)
+				console.log(this.enddate)
 			}
 		}
 
@@ -36,6 +98,7 @@
 	overflow: hidden;
 }
 .text-box {
+	position: relative;
 	padding-bottom: 125rpx;
 	padding-right: 75rpx;
 	border: 1upx solid #eee;
@@ -47,5 +110,27 @@
   position: absolute;
   left: 540rpx;
   top: 430rpx;
+}
+.begin,.end{
+	border: 1upx solid #eee;
+}
+.begin-word,.end-word{
+	position: relative;
+	top: 20rpx;
+	left: 45rpx;
+}
+.begin-input,.end-input{
+	position: relative;
+	left: 540rpx;
+	top: -26rpx;
+}
+.submit-btn{
+	width: 80%;
+	margin: 0 auto 40upx;
+	background: #0055ff;
+	border-radius: 80upx;
+	color: #fff;
+	font-size: 32upx;
+	top: 25rpx;
 }
 </style>
