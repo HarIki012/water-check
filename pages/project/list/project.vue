e44641636da9d328db082404051e2e939f8997f3
Erertao <1793009453@qq.com>
2023-04-06 11:49:13 +0800

    项目新建功能ver1.6(样式修改）

--- pages/project/list/project.vue
+++ pages/project/list/project.vue
@@ -4,7 +4,7 @@
 			<view class="projectStyle">
 				<picker class="centerStyle" @change="projectSelect" :range="projectChoose">
 					<label>{{ projectselectName }}</label>
-					<text class="show-or-noshow iconfont icon icon-xiangxia"></text>
+					<text class="iconfont icon icon-xiangxia"></text>
 				</picker>
 			</view>
 			<view class="selectStyle">
@@ -45,12 +45,7 @@
 					项目名称
 				</view>
 				<view class="popup-use1">
-					<input class="oldStyle" style="padding-left: 20rpx;" type="text" v-model="newProjectname" confirm-type="done" @confirm="oldInput">
-				</view>
-				<view class="popup-use1">
-					<view class="error" v-if="errorTips1===1">
-						<text class="error-text">输入的项目名称不能为空！</text>
-					</view>
+					<input class="oldStyle" style="padding-left: 20rpx;" placeholder="请输入项目名称" type="text" v-model="newProjectname" confirm-type="done" @confirm="oldInput">
 				</view>
 				
 				<view class="popup-use1">
@@ -58,33 +53,32 @@
 					所属巡检活动
 				</view>
 				<view class="popup-use1">
-					<view class="oldStyle">
-						<picker class="centerStyle" @change="dateSelect" :range="dateChoose">
-							<label >{{ dateName }}</label>
+					<view class="project-time">
+						<picker style="width: 90%;" @change="dateSelect" :range="dateChoose">
+							<label class="label-style">{{ dateName }}</label>
+						</picker>
+						<picker @change="dateSelect" :range="dateChoose">
+							<text class="iconfont icon icon-xiangxia"></text>
 						</picker>
 					</view>
 				</view>
-				<view class="popup-use1">
-					<view class="error" v-if="errorTips2===1">
-						<text class="error-text">请选择所属巡检活动！</text>
-					</view>
-				</view>
 				
-				<view class="popup-use">
-					<view style="width: 80%;text-align: left;">
+				<view class="popup-use1">
+					<view style="width: 70%;text-align: left;">
 						经纬度
 					</view>
-					<view style="width: 20%;text-align: right;color: blue;" @tap="addressGet">
+					<view style="width: 30%;text-align: right;color: blue;" @tap="addressGet">
+						<text class="iconfont icon icon-xiangxia"></text>
 						获取
 					</view>
 				</view>
 				<view class="popup-use1">
 					<input class="oldStyle" style="padding-left: 20rpx;" v-if="upData" type="text" v-model="addressData" confirm-type="done">
 				</view>
-		
-				<button class="buttonStyle"  type="primary" @tap="closePop('top')">
-					确定
-				</button>
+				
+				<button v-if="this.newProjectname && this.dateName !== this.firstName" class="buttonStyle" type="primary" @tap="closePop('top')">确定</button>	
+				<button v-else class="buttonStyle" type="primary" disabled="true" @tap="closePop('top')">确定</button>
+				
 			</view>
 		</uni-popup>
 	</view>
@@ -123,9 +117,6 @@ import { warn } from "vue"
 				],
 				newProjectname: '',
 				isNew: '',
-				errorTips1: '',
-				errorTips2: '',
-				errorTips3: '',
 				blankSpace: '',
 				dateChoose: ['2023年第一次检查','2023年第二次检查','2023年第三次检查','2023年第四次检查','2023年第五次检查','2023年第六次检查'],
 				projectChoose: ['区域选择', '待检查', '检查中', '已检查', '已中止'],
@@ -133,10 +124,11 @@ import { warn } from "vue"
 				dateIndex: 0,
 				projectselectName: '区域选择',
 				projectnameSearch: '',
-				dateName:'---请选择---',
-				firstName:'---请选择---',
+				dateName:'请选择巡检活动',
+				firstName:'请选择巡检活动',
 				addressMessage: '',
 				addressData: '',
+				buttonUse: false,
 				upData: true
 			}
 		},
@@ -152,6 +144,13 @@ import { warn } from "vue"
 					arr = arr.filter(item => item.status.includes(this.projectselectName))
 				}
 				return arr
+			},
+			activeButton(){
+				if (this.newProjectname && this.dateName !== this.firstName) {
+					this.buttonUse = false
+				} else {
+					this.buttonUse = true
+				}
 			}
 		},
 		methods: {
@@ -161,21 +160,7 @@ import { warn } from "vue"
 				})
 			},
 			closePop() {
-				if (!this.newProjectname){
-					this.errorTips1 = 1
-				} else {
-					this.errorTips1 = 0
-				}
-				if (this.dateName === this.firstName) {
-					this.errorTips2 = 1
-				} else {
-					this.errorTips2 = 0
-				}
-				if (this.newProjectname && this.dateName !== this.firstName) {
-					this.newProjectname = this.blankSpace
-					this.addressData = this.blankSpace
-					this.$refs['popup'].close()
-				}
+				this.$refs['popup'].close()
 			},
 			closeButton(){
 				this.$refs['popup'].close()
@@ -235,182 +220,6 @@ import { warn } from "vue"
 	}
 </script>
 
-<style lang="scss">
-	.backgroundStyle{
-		width: 100%;
-		height: 100%;
-		background-color: #f2f5f8;
-	}
-	.titleStyle{
-		text-align: center;
-		margin-top: 30rpx;
-		font-size: 40rpx;
-		margin-bottom: 20rpx;
-		border-bottom: 1rpx solid lightslategray;
-		padding-bottom: 20rpx;
-	}
-	.logo {
-		margin-left: 10rpx;
-		margin-right: 10rpx;
-		height: 50rpx;
-		width: 50rpx;
-		display: flex;
-		font-size: 45rpx;
-	}
-	.pop{
-		width: 100%;
-		background-color: white;
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-	}
-	.popup-use{
-		padding: 10rpx 30rpx;
-		padding-top: 30rpx;
-		width: 530rpx;
-		display: flex;
-		justify-content: left;
-		letter-spacing: 2rpx;
-	}
-	.popup-use1{
-		padding: 10rpx 30rpx;
-		padding-top: 10rpx;
-		width: 530rpx;
-		display: flex;
-		justify-content: left;
-		letter-spacing: 2rpx;
-	}
-	.error-icon{
-		font-size: 28rpx;
-		padding-left: 40rpx;
-		display: inline-block;
-		width: 2rpx;
-		height: 30rpx;
-		line-height: 30rpx;
-		border-radius: 20rpx;
-		background-color: #fe3535;
-		color: #FFFFFF;
-		margin-right: 20rpx
-	}
-	.error-text{
-		font-size: 28rpx;
-		padding-top: 10rpx;
-		color: red;
-	}
-	.oldStyle{
-		border: 1rpx solid gray;
-		width: 100%;
-		height: 70rpx;
-	}
-	.projectSearch{
-		padding-top: 30rpx;
-		width: 100%;
-		height: 80rpx;
-		display: flex;
-		justify-content: left;
-		letter-spacing: 2rpx;
-		background-color: #f2f5f8;
-		position: sticky;
-		top: 0;
-	}
-	.projectStyle{
-		margin-left: 3%;
-		border-radius: 5px;
-		text-align: center;
-		align-items: center;
-		flex-direction: row;
-		margin-right: 1%;
-		width: 25%;
-		height: 70rpx;
-		position: sticky;
-		top: 0;
-	}
-	.centerStyle{
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		height: 100%;
-		position: sticky;
-	}
-	.selectStyle{
-		border-radius: 15px;
-		width: 55%;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		height: 70rpx;
-		position: sticky;
-		background-color: white;
-	}
-	.searchButton{
-		margin-left: 25rpx;
-		display: flex;
-		height: 70rpx;
-		justify-content: center;
-		align-items: center;
-		color: blue;
-	}
-	.buttonStyle{
-		width: 300rpx;
-		height: 88rpx;
-		padding-top: 0rpx;
-		font-size: 35rpx;
-		text-align: center;
-		margin-top: 30rpx;
-		margin-bottom: 40rpx;
-	}
-	.tableStyle{
-		width: 90%;
-		margin-top: 25rpx;
-		margin-left: 5%;
-		display: flex;
-		flex-direction: column;
-		height: 250rpx;
-		background-color: white;
-	}
-	.logoItem{
-		width: 100rpx;
-		height: 100rpx;
-		font-size: 60rpx;
-		border-radius: 100rpx;
-		margin-right: 20rpx;
-		display: flex;
-		justify-content: center;
-		background-color: aliceblue;
-		align-items: center;
-	}
-	.tableContent{
-		padding-left: 20rpx;
-		padding-top: 20rpx;
-		padding-right: 20rpx;
-		padding-bottom: 20rpx;
-		display: flex;
-		flex-direction: row;
-		height: 80%;
-		font-size: 35rpx;
-	}
-	.newStyle{
-		width: 100%;
-		height: 100rpx;
-		font-size: 35rpx;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		background-color: aqua;
-	}
-	.statusStyle{
-		padding-left: 20rpx;
-		padding-top: 20rpx;
-		padding-right: 20rpx;
-		padding-bottom: 20rpx;
-		height: 20%;
-		text-align: right;
-		font-family: '阿里巴巴普惠体 2.0 65 Medium';
-	}
-	/* CDN 服务仅供平台体验和调试使用，平台不承诺服务的稳定性，企业客户需下载字体包自行发布使用并做好备份。 */
-	@font-face {
-	  font-family: "阿里巴巴普惠体 2.0 65 Medium";font-weight: 500;src: url("//at.alicdn.com/wf/webfont/R3pFSnXNf5DJ/ySEF0qX9msER.woff2") format("woff2"),
-	  url("//at.alicdn.com/wf/webfont/R3pFSnXNf5DJ/wYtU5udvc31K.woff") format("woff");
-	  font-display: swap;
-	}
+<style lang="scss" scoped>
+@import url('project.scss');
 </style>
